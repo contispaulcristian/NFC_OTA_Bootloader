88ee2838542c6c70ed78079a7300f6eb86b21a0c
Paul Contis <paul.contis@diyforgeeks.com>
2026-07-13 17:27:52 +0300

    Test code for NFC chip

--- 03_Bootloader/src/hal/st25dv64k_driver.c
+++ 03_Bootloader/src/hal/st25dv64k_driver.c
@@ -12,12 +12,17 @@
  * MODULE HEADER
  *----------------------------------------------------------------------------*/
 #include "st25dv64k_driver.h"
-#include "i2c_drv.h"
-#include "config.h" // To get ST25DV_SYS_ADDR
 
 /*----------------------------------------------------------------------------*
  * PRIVATE INCLUDES
  *----------------------------------------------------------------------------*/
+#include "i2c_drv.h"
+#include "config.h"
+
+#include "port_drv.h" /*debug purpose*/
+#include <util/delay.h>
+
+#include "../debug.h"
 
 /*----------------------------------------------------------------------------*
  * PRIVATE CONSTANTS & MACROS
@@ -45,7 +50,16 @@
 St25_Status_t St25_Init(void)
 {
     St25_Status_t Status = ST25_OK;
+    /* Reset Mailbox */
 
+    /* Configure GPO */
+    /* Write ST25_GPO_CTRL_REG: Enable GPO and Mailbox Message interrupt */
+
+    /* nable Mailbox (Last step) */
+    /* Write ST25_MAILBOX_CTRL_REG: Enable Mailbox */
+
+    /* Verification of Configuration */
+    /* Read MAILBOX_CTRL and GPO_CTRL to ensure they hold the expected values */
     return Status;
 }
 
@@ -55,10 +69,44 @@ St25_Status_t St25_Init(void)
  * @param Length Number of bytes to read.
  * @return Status
  */
-St25_Status_t St25_Read_Mailbox(uint8_t *Buffer, uint16_t Length)
+St25_Status_t St25Dv_Read_Mailbox(uint8_t *Buffer, uint16_t Length)
 {
     St25_Status_t Status = ST25_OK;
+    /* We need to send the 16-bit register address: 0x2008 */
+    uint8_t reg_addr[2];
+    reg_addr[0] = (uint8_t)(HAL_ST25_HOST_PUT_MSG_REG >> 8);   // 0x20
+    reg_addr[1] = (uint8_t)(HAL_ST25_HOST_PUT_MSG_REG & 0xFF); // 0x08
 
+    /*Dummy Write the Register Address using REPEATED START */
+    I2c_Data_Transfer_t write_transfer = {
+        .Slave_Addr = 0x53U,
+        .Data       = reg_addr,
+        .Len        = 2,
+        .Action     = I2C_RESTART /* CRITICAL: Do not release the bus! */
+    };
+
+    if (Mcal_I2c_Write(&write_transfer) != I2C_OK)
+    {
+        //Blink_Debug(2, 400);
+        Status = ST25_ERR_COMM;
+    }
+
+    /* Read the payload using STOP */
+    if (Status == 0U)
+    {
+        I2c_Data_Transfer_t read_transfer = {
+            .Slave_Addr = 0x53U,
+            .Data       = Buffer,
+            .Len        = Length,
+            .Action     = I2C_STOP /* Finish transaction and release bus */
+        };
+
+        if (Mcal_I2c_Read(&read_transfer) != I2C_OK)
+        {
+            Status = ST25_ERR_COMM;
+            //Blink_Debug(2, 400);
+        }
+    }
     return Status;
 }
 
@@ -77,33 +125,33 @@ St25_Status_t ST25DV_Write_Ack(uint8_t Data)
 /*----------------------------------------------------------------------------*
  * PRIVATE FUNCTION IMPLEMENTATIONS
  *----------------------------------------------------------------------------*/
-/**
- * @brief Register Accessor for System Config (Fixed Address)
- */
-static St25_Status_t St25DV_Write_System_Reg(uint16_t Reg_Addr, uint8_t Data)
-{
-}
+// /**
+//  * @brief Register Accessor for System Config (Fixed Address)
+//  */
+// static St25_Status_t St25DV_Write_System_Reg(uint16_t Reg_Addr, uint8_t Data)
+// {
+// }
 
-/**
- * @brief Register Accessor for Dynamic Data (Mailbox/Status)
- */
-static St25_Status_t St25DV_Write_Dynamic_Reg(uint16_t Reg_Addr, const uint8_t *Data, uint16_t Len)
-{
-}
+// /**
+//  * @brief Register Accessor for Dynamic Data (Mailbox/Status)
+//  */
+// static St25_Status_t St25DV_Write_Dynamic_Reg(uint16_t Reg_Addr, const uint8_t *Data, uint16_t Len)
+// {
+// }
 
-/**
- * @brief Register Accessor for Dynamic Data (Mailbox/Status)
- */
-/* 2. Low-level read from a system register */
-static St25_Status_t St25DV_Read_Reg(uint16_t Reg_Addr, uint8_t *Data);
+// /**
+//  * @brief Register Accessor for Dynamic Data (Mailbox/Status)
+//  */
+// /* 2. Low-level read from a system register */
+// static St25_Status_t St25DV_Read_Reg(uint16_t Reg_Addr, uint8_t *Data);
 
-/**
- * @brief Mailbox Status check
-*/
-static bool St25DV_Is_Mailbox_Ready(void)
-{
-}
+// /**
+//  * @brief Mailbox Status check
+// */
+// static bool St25DV_Is_Mailbox_Ready(void)
+// {
+// }
 
-St25_I2c_Transport(uint8_t Target_Addr, uint16_t Reg, uint8_t *Data, uint16_t Len)
-{
-}
+// St25_I2c_Transport(uint8_t Target_Addr, uint16_t Reg, uint8_t *Data, uint16_t Len)
+// {
+// }
--- 03_Bootloader/src/hal/st25dv64k_driver.h
+++ 03_Bootloader/src/hal/st25dv64k_driver.h
@@ -18,10 +18,19 @@
 /*----------------------------------------------------------------------------*
  * PUBLIC CONSTANTS & MACROS
  *----------------------------------------------------------------------------*/
-/* --- ST25DV FTM Dynamic Registers --- */
-#define ST25DV_REG_GPO_DYN     0x2000U
-#define ST25DV_REG_MB_CTRL_DYN 0x2029U
-#define ST25DV_REG_MB_FIFO     0x202BU
+/* System Registers */
+#define HAL_ST25_GPO_CTRL_REG     0x0002U
+#define HAL_ST25_MAILBOX_CTRL_REG 0x0003U
+
+/* Dynamic Registers */
+//#define HAL_ST25DV_REG_GPO_DYN    0x2000U
+#define HAL_ST25_HOST_PUT_MSG_REG 0x2008U
+#define HAL_ST25_MBLEN_DYN_REG    0x2006U
+
+/* Bitmasks */
+#define ST25_MB_EN_BIT            0x01U
+#define ST25_GPO_EN_BIT           0x01U
+#define ST25_MB_MSG_ON_BIT        0x02U
 
 /*----------------------------------------------------------------------------*
  * PUBLIC TYPEDEFS & STRUCTURES
@@ -40,7 +49,9 @@ typedef enum
  *----------------------------------------------------------------------------*/
 St25_Status_t ST25DV_Init(void);
 
-St25_Status_t ST25DV_Read_Mailbox(uint8_t *Buffer, uint16_t Len);
-St25_Status_t St25_Write_Ack(uint8_t Data);
+St25_Status_t St25Dv_Read_Mailbox(uint8_t *Buffer, uint16_t Len);
+St25_Status_t St25Dv_Write_Ack(uint8_t Data);
+
+bool St25_Is_Data_Available(void);
 
 #endif /* ST25DV64K_DRIVER_H */
